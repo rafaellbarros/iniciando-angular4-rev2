1cfb66157684b70cace8998bd1336f2c3a98886d
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-10 23:26:14 -0200

    Pontapé inicial

--- angular-hello-world/src/app/app.component.ts
+++ angular-hello-world/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tarefas';
+  taks = [];
+  task = '';
+
+  add() {
+    this.taks.push(this.task);
+  }
+
 }
