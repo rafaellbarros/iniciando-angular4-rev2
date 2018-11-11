2ad03ea672f2481da37777c7d5dd91bb83bd5f55
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-11 10:00:03 -0200

    Trabalhando com diretivas

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { FormatCurrencyPipe } from './format-currency.pipe';
 import { FormatDatePipe } from './format-date.pipe';
+import { MyTaskDirective } from './my-task.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
     FormatCurrencyPipe,
-    FormatDatePipe
+    FormatDatePipe,
+    MyTaskDirective
   ],
   imports: [
     BrowserModule,
