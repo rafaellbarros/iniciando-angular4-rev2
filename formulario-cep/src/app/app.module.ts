67291d231cc97d845a62b2dc9b8978b2843d730a
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-11 23:51:30 -0200

    Criando compomente cep

--- formulario-cep/src/app/app.module.ts
+++ formulario-cep/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CepComponent } from './cep/cep.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CepComponent
   ],
   imports: [
     BrowserModule
