259191e5951f73fa6564388502f5c3b0add6b757
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-12 22:39:40 -0200

    Adicionando mascara cep

--- formulario-cep/src/app/app.module.ts
+++ formulario-cep/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import {NgxMaskModule} from 'ngx-mask';
 
 import { AppComponent } from './app.component';
 import { CepComponent } from './cep/cep.component';
@@ -16,7 +17,8 @@ import { CepService } from './cep.service';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [ CepService ],
   bootstrap: [AppComponent]
